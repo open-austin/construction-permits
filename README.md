e912cbabb2458556fa4936270de7a2750207ece6
Luqmaan <ldawoodjee@gmail.com>
2015-12-13 11:15:49 -0600

    PEP8 and such

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Data scraped from http://www.austintexas.gov/oss_permits/permit_report.cfm.
 
+## Usage
+
+Get today's construction permits and store them in `data`:
+
+```
+python dank-eshet.py
+```
+
 ## License
 
 The code for this repository has been released into the public domain by Open Austin via the Unlicense.
--- dank-eshet.py
+++ dank-eshet.py
@@ -10,13 +10,16 @@ import requests
 import geocoder
 import logging
 import csv
-import pdb
+
+DELIM = '|'
+address_dict = dict()
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log = logging.getLogger('requests.packages.urllib3')
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
+
 
 def get_data(today):
     print('Getting data from {} to {}'.format(today, today))
@@ -29,64 +32,68 @@ def get_data(today):
     res.raise_for_status()
     return res.content
 
+
 def parse_table_html(data):
     data = str(data)
-    data = data.replace("\\n","")
-    data = data.replace("\\t","")
-    data = data.replace("b'","")
-    data = data.replace("<table border=\"1\">","")
-    data = data.replace("</table>","")
-    data = data.replace("<th>","")
-    data = data.replace("</th>","|")
-    data = data.replace("<td>","")
-    data = data.replace("</td>",delim)
-    data = data.replace("<tr>","")
-    data = data.replace("</tr>","\n")
+    data = data.replace('\\n', '')
+    data = data.replace('\\t', '')
+    data = data.replace('b', '')
+    data = data.replace('<table border="1">', '')
+    data = data.replace('</table>', '')
+    data = data.replace('<th>', '')
+    data = data.replace('</th>', '|')
+    data = data.replace('<td>', '')
+    data = data.replace('</td>', DELIM)
+    data = data.replace('<tr>', '')
+    data = data.replace('</tr>', '\n')
     return data
-    
+
+
 def geocode_data(data):
     geocoded_data = []
-    reader = csv.DictReader(data.split('\n'),  delimiter='|')
+    reader = csv.DictReader(data.split('\n'), delimiter=DELIM)
     fieldnames = reader.fieldnames + ['lat'] + ['lng'] + ['accuracy'] + ['city'] + ['postal_code'] + ['state'] + ['county']
-    
-    #  create list of unique addresses 
+
+    #  create list of unique addresses
     for row in reader:
         address = row['permit_location']
-        if type(address) == str: #  normalize addresses and skip empty address fields
+        if type(address) == str:  # normalize addresses and skip empty address fields
                 address = address.upper().strip()
         else:
-            continue #  data with invalid address is skipped
+            continue  # data with invalid address is skipped
 
         if address not in address_dict:
-            time.sleep(.5) #  delay for .5 seconds between lookup requests. the api limit is 5 requests per second
-            found_address = geocoder.google(address +', Austin, TX')
+            time.sleep(.5)  # delay for .5 seconds between lookup requests. the api limit is 5 requests per second
+            found_address = geocoder.google(address + ', Austin, TX')
             address_dict[address] = found_address
-        
+
         if address in address_dict:
             if address_dict[address].lat:
                 row['lat'] = address_dict[address].lat
                 row['lng'] = address_dict[address].lng
                 row['accuracy'] = address_dict[address].accuracy
-                row["city"] = address_dict[address].city
-                row["county"] = address_dict[address].county
-                row["state"] = address_dict[address].state
-                row["postal_code"] = address_dict[address].postal
-       
-        geocoded_data.append(row) 
-    print(str(len(address_dict)) + " unique addresses geocoded")
+                row['city'] = address_dict[address].city
+                row['county'] = address_dict[address].county
+                row['state'] = address_dict[address].state
+                row['postal_code'] = address_dict[address].postal
+
+        geocoded_data.append(row)
+    print(str(len(address_dict)) + ' unique addresses geocoded')
     return (geocoded_data, fieldnames)
+
 
 def write_data(data, fieldnames):
     year = arrow.now().format('YYYY')
     today = arrow.now().format('MM-DD-YY')
     fname = 'data/{}/permit_report_{}_{}.xls'.format(year, today, today)
-    if not os.path.exists("data/" + year):
-        os.makedirs("data/" + year)
+    if not os.path.exists('data/' + year):
+        os.makedirs('data/' + year)
 
     with open(fname, 'w+') as fh:
-        writer = csv.DictWriter(fh, fieldnames, delimiter = '|', lineterminator='\n')
+        writer = csv.DictWriter(fh, fieldnames, delimiter=DELIM, lineterminator='\n')
         writer.writeheader()
         writer.writerows(data)
+
 
 def main(today):
     print('Fetching permit reports for {}'.format(today))
@@ -95,7 +102,6 @@ def main(today):
     data = geocode_data(data)
     write_data(data[0], data[1])
 
-delim = "|"
-address_dict = dict()
-today = arrow.now().format('MM/DD/YYYY')
-main(today)
+if __name__ == '__main__':
+    today = arrow.now().format('MM/DD/YYYY')
+    main(today)
